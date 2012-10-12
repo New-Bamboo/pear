b71981372183ce8c2a98cbb2dca2b0e603785469
Mark Evans <mark@new-bamboo.co.uk>
2012-10-12 17:07:05 +0100

    oops needed string not symbol key

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 
   EVENT_TYPES.each do |event_type|
     define_method event_type do
-      Event.new(event_type, event_params.merge(source: current_user)).send!
+      Event.new(event_type, event_params.merge('source' => current_user)).send!
       render nothing: true, status: 201
     end
   end
