5720f0712e68236447a474a17f0a3945d9bd3b62
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-10-12 10:34:41 +0100

    rails yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pear::Application.config.secret_token = '374530a8665748f724fa44cd32eb416122ab18ee4e96ab7969cde304ae59e0803acadea6ccb010317aefe414771ca1c65f158930ed01ca05a7d9b757e8cbab7b'
